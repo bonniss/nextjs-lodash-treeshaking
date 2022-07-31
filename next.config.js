53878a813981f8589ed48d566f7e6fb119807a5d
trung.phamduy User <duytrung.tcu@gmail.com>
2022-08-01 01:17:08 +0700

    config lodash es

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+const withTM = require('next-transpile-modules')(['lodash-es'])
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  [withTM]
+], nextConfig)
